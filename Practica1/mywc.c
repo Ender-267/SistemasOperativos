36a0d130c091f39a23ab3cbcf62581f6a67f2fe7
Ender-267 <albertolozano2004@gmail.com>
2024-02-16 11:00:41 +0100

    ej1 p1 borrador

--- Practica1/mywc.c
+++ Practica1/mywc.c
@@ -1,7 +1,22 @@
 //P1-SSOO-23/24
 
+#include <unistd.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#define TRUE 1
+#define FALSE 0
 
+char buffer[1];
+int fd;
+int ret;
+int bytesRead = -1;
+int byteCounter = 0;
+int lineCounter = 0;
+int wordCounter = 0;
+int lastType = 0; 
+char debug = 0;
 
 int main(int argc, char *argv[])
 {
@@ -11,10 +26,42 @@ int main(int argc, char *argv[])
 		printf("Too few arguments\n");
 		return -1;
 	}
-
-	
-
-
-	return 0;
+	if(argc >= 3){
+		char debug = *argv[2];
+		printf("DEBUG\n");
+	}
+	int fd = open(argv[1], O_RDONLY);
+	if (fd == -1){
+		printf("Open error");
+		return -1;
+	}
+	while(bytesRead != 0){
+		bytesRead = read(fd, buffer, 1);
+		if(bytesRead == -1){
+			printf("Read error");
+			return -1;
+		}
+		//DEBUG
+		if(debug=="1"){
+			printf("%i    %c\n",byteCounter, buffer[0]);
+		}
+		switch(buffer[0]){
+			case '\n':
+				lineCounter++;
+				break;
+			case ' ':
+				wordCounter++;
+				break;
+			case '\t':
+				wordCounter++;
+				break;
+			default:
+				break;
+		}
+		byteCounter = byteCounter + bytesRead;
+	}
+	printf("%i %i %i\n", lineCounter, wordCounter ,byteCounter);
+	int ret = close(fd);
+	return ret;
 }
 
